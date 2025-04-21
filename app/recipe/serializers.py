c999b94456641a41900ef44a0e2e255a4b7a8478
nimaabf <abf.nima@gmail.com>
2025-04-22 00:27:15 +0330

    Added Recipe Model+serilizer+test

---
+++ app/recipe/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from core.models import Recipe
+from django.conf import settings
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
