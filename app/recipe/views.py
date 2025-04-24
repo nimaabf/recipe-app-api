290e715ae1b4616c877d9f87141d3895e4be3d94
nimaabf <abf.nima@gmail.com>
2025-04-25 02:19:42 +0330

    Added Ingredients Views & Serializer

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 from recipe.serializers import TagSerializer
 
@@ -36,4 +36,14 @@ class TagViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.DestroyM
 
     def get_queryset(self):
         """Filter QuerySet for Authened User"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
